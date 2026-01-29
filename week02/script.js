96c05e5e4b24d1b12c8784e3ac8948613b70e172
Natasha Chieng <natasha.chieng@uconn.edu>
2026-01-29 11:50:48 -0500

    this is my week 2 code.

---
+++ week02/script.js
@@ -0,0 +1,24 @@
+var week = 2;
+
+console.log("Hello, this is week " +week + ".");
+
+//alert("Hello, this is really week 2");
+
+var today = "thursday";
+//console.log(today);
+var tomorrow = "Friday";
+
+//console.log("today is thursday");
+//console.log("tomorrow is Friday");
+
+var greeting = "Today is " + today + "."
+
+console.log(greeting);
+
+var nextWeek = week + 1;
+console.log("Next week will be week " + nextWeek);
+
+//var myName = "meep"
+
+var myName = prompt("What is your name?");
+alert("My name is " + myName );
